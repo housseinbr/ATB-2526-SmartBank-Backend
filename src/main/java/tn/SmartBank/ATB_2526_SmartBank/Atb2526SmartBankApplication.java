43fb9e6a27beefeb7b98a227872b8d970ef43afe
housseinbr <80652273+housseinbr@users.noreply.github.com>
2026-07-01 21:50:00 +0200

    test

---
+++ src/main/java/tn/SmartBank/ATB_2526_SmartBank/Atb2526SmartBankApplication.java
@@ -0,0 +1,12 @@
+package tn.SmartBank.ATB_2526_SmartBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atb2526SmartBankApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Atb2526SmartBankApplication.class, args);
+        System.out.println("hello");
+	}
+}
---
+++ src/test/java/tn/SmartBank/ATB_2526_SmartBank/Atb2526SmartBankApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.SmartBank.ATB_2526_SmartBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atb2526SmartBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
